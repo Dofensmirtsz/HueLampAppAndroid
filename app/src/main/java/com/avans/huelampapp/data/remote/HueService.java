025bdc197658790e1ee348ebfc875dae765d0695
Tim van Lieshout <dofensmirtsz@gmail.com>
2017-10-28 23:51:36 +0200

    imports

--- app/src/main/java/com/avans/huelampapp/data/remote/HueService.java
+++ app/src/main/java/com/avans/huelampapp/data/remote/HueService.java
@@ -9,6 +9,8 @@ import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
